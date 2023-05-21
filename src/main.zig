0a49d5043e89829e4075840f3cd21b16102b88fe
Albert Yu <albert.yu@gmail.com>
2023-05-21 17:42:54 -0400

    Add feedforward test

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,13 @@ pub fn main() !void {
     // do nothing
 }
 
-test "multiply_add test" {
+test "feedforward test" {
     const err_tolerance = 1e-9;
     const LAYER_SIZE = 3;
     var test_x = [LAYER_SIZE]f32{ 1, 2, 3 };
     var weights = [LAYER_SIZE]f32{ 1, 2, 3 };
     var test_layer = nn.NetworkLayer{ .weights = &weights, .bias = 0.1443 };
-    var result = test_layer.multiply_add(&test_x);
-    var expected: f32 = 14.1443;
+    var result = test_layer.feedforward(&test_x);
+    var expected: f32 = 0.999999284;
     try std.testing.expectApproxEqRel(expected, result, err_tolerance);
 }
--- src/network.zig
+++ src/network.zig
@@ -1,3 +1,9 @@
+const std = @import("std");
+
+fn sigmoid(val: f32) f32 {
+    return 1 / (1 + std.math.exp(-val));
+}
+
 pub const NetworkLayer = struct {
     weights: []f32,
     bias: f32,
@@ -7,12 +13,17 @@ pub const NetworkLayer = struct {
     }
 
     /// w * x + b
-    pub fn multiply_add(self: NetworkLayer, x: []const f32) f32 {
+    fn multiply_add(self: NetworkLayer, x: []const f32) f32 {
         var result: f32 = 0;
         for (self.weights) |weight, i| {
             const other_val = x[i];
             result += weight * other_val;
         }
         return result + self.bias;
+    }
+
+    pub fn feedforward(self: NetworkLayer, x: []const f32) f32 {
+        var first_result = self.multiply_add(x);
+        return sigmoid(first_result);
     }
 };
