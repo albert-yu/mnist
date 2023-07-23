1c1ac0ef254013341fdf5257d024c5b6fa292058
Albert Yu <albert.yu@gmail.com>
2023-07-23 16:03:05 -0400

    Remove debug print

--- src/network.zig
+++ src/network.zig
@@ -214,7 +214,6 @@ fn copy_image_data(input: []const u8, output: []f32) void {
 pub fn make_mnist_data_points(allocator: std.mem.Allocator, x: []const u8, x_chunk_size: usize, y: []const u8, y_output_size: usize) ![]DataPoint {
     const result = try allocator.alloc(DataPoint, x.len / x_chunk_size);
     var i: usize = 0;
-    std.debug.print("x.len: {}\n", .{x.len});
     var idx: usize = 0;
     // assuming x / x_chunk_size and y.len are the same
     while (i < x.len) {
